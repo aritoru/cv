afe79d37e13d0fbddb162a737955442cd2211074
aritoru <aritoru@marlin.(none)>
2013-02-28 22:23:48 +0100

    new social icons

--- web/social.php
+++ web/social.php
@@ -1,11 +1,15 @@
 <!-- social -->
 <div id="social-bar">
     <ul>
-        <li><a href="https://www.facebook.com/aritoru"  title="My Page on FB"><img src="img/social/facebook_32.png"  alt="Facebook" /></a></li>
-        <li><a href="https://twitter.com/aritoru" title="Follow my tweets"><img src="img/social/twitter_32.png"  alt="Twitter" /></a></li>
-        <li><a href="https://plus.google.com/115317823912032757327"  title="Add me to your circles"><img src="img/social/google_plus_32.png" alt="Google Plus" /></a></li>
-        <li><a href="http://es.linkedin.com/in/aitoribaceta"  title="My LinkedIn Profile"><img src="img/social/linkedin_32.png" alt="LinkedIn" /></a></li>        
-        <li><a href="mailto:aitor.ibaceta@gmail.com"  title="Email Me"><img src="img/social/email_32.png" alt="Email" /></a></li>        
+        <li><a href="https://www.facebook.com/aritoru"  title="My Page on FB"><img src="img/social/facebook.png"  alt="Facebook" /></a></li>
+        <li><a href="https://twitter.com/aritoru" title="Follow my tweets"><img src="img/social/twitter.png"  alt="Twitter" /></a></li>
+        <li><a href="https://plus.google.com/115317823912032757327"  title="Add me to your circles"><img src="img/social/google_plus.png" alt="Google Plus" /></a></li>
+        <li><a href="http://es.linkedin.com/in/aitoribaceta"  title="My LinkedIn Profile"><img src="img/social/linkedin.png" alt="LinkedIn" /></a></li>        
+        <li><a href="https://github.com/aritoru"  title="My Projects"><img src="img/social/github.png" alt="Github" /></a></li>        
+        <li><a href="http://instagram.com/aritoru"  title="My Photos"><img src="img/social/instagram.png" alt="Instagram" /></a></li>        
+        <li><a href="skype:g0rk4m"  title="Skype Me"><img src="img/social/skype.png" alt="Skype" /></a></li>                
+        <li><a href="mailto:aitor.ibaceta@gmail.com"  title="Email Me"><img src="img/social/email.png" alt="Email" /></a></li>        
+        
     </ul>
 </div>
 <!-- ENDS social -->
